4e838bd4b1a03e13e88c0d39adb67ef8a4940ca9
Ryan Alexander <ralexander15@sfs02066mac.sidwell.edu>
2015-03-16 10:08:56 -0400

    added a test class

---
+++ RATest.h
@@ -0,0 +1,13 @@
+//
+//  RATest.h
+//  Git Test
+//
+//  Created by Ryan Alexander on 3/16/15.
+//  Copyright (c) 2015 Ryan Alexander. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RATest : NSObject
+
+@end
---
+++ RATest.m
@@ -0,0 +1,13 @@
+//
+//  RATest.m
+//  Git Test
+//
+//  Created by Ryan Alexander on 3/16/15.
+//  Copyright (c) 2015 Ryan Alexander. All rights reserved.
+//
+
+#import "RATest.h"
+
+@implementation RATest
+
+@end
